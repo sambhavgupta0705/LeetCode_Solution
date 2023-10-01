2e4a63a84f2b6ccd146cc5aa2aa4b33400fe2f34
Sambhav Gupta <81870866+sambhavgupta0705@users.noreply.github.com>
2023-10-01 10:24:55 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -10,31 +10,50 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-       
-     
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        vector<int> distTo(V,INT_MAX);
+        set<pair<int,int>> st; 
+
+        // Initialising dist list with a large number to
+        // indicate the nodes are unvisited initially.
+        // This list contains distance from source to the nodes.
+        vector<int> dist(V, 1e9); 
         
-        distTo[S]=0;
-        pq.push({0,S});
+        st.insert({0, S}); 
+
+        // Source initialised with dist=0
+        dist[S] = 0;
         
-        while(!pq.empty()){
-            int node=pq.top().second;
-            int dis=pq.top().first;
+        // Now, erase the minimum distance node first from the set
+        // and traverse for all its adjacent nodes.
+        while(!st.empty()) {
+            auto it = *(st.begin()); 
+            int node = it.second; 
+            int dis = it.first; 
+            st.erase(it); 
             
-            pq.pop();
-            
-            for(auto it:adj[node]){
-                int v=it[0];
-                int w=it[1];
-                 if (dis + w < distTo[v])
-                {
-                    distTo[v] = dis + w;
-                    pq.push({dis + w, v});
-                }
+            // Check for all adjacent nodes of the erased
+            // element whether the prev dist is larger than current or not.
+            for(auto it : adj[node]) {
+                int adjNode = it[0]; 
+                int edgW = it[1]; 
+                
+                if(dis + edgW < dist[adjNode]) {
+                    // erase if it was visited previously at 
+                    // a greater cost.
+                    if(dist[adjNode] != 1e9) 
+                        st.erase({dist[adjNode], adjNode}); 
+                        
+                    // If current distance is smaller,
+                    // push it into the queue
+                    dist[adjNode] = dis + edgW; 
+                    st.insert({dist[adjNode], adjNode}); 
+                 }
             }
         }
-           return distTo;
+        // Return the list containing shortest distances
+        // from source to all the nodes.
+        return dist; 
+       
+       
     }
 };
 
